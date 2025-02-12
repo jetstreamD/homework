edd53fad5fe6c30ece482f6479b1ab6b0dcc3a67
jetstreamD <tepetovdemyan537@gmail.com>
2025-02-13 00:45:22 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,9 +1,36 @@
+
+/////////////////////// HAMBURGER ////////////////////////
 const hamburger = document.querySelector('.hamburger')
 const hamburgerMenu = document.querySelector('.header__nav')
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     hamburgerMenu.classList.toggle('active')
 })
+
+/////////////////////// MODAL WINDOW /////////////////////
+
+const modalWin = document.querySelector('.modal')
+const openModal = document.querySelector('.about__img-btn')
+const closeModal = document.querySelector('.modal__cancel')
+const closeModalOutside = document.querySelector('.modal__blackout')
+const modalArray = [openModal, closeModal, closeModalOutside]
+modalArray.forEach((element => {
+    element.addEventListener('click', () => {
+        modalWin.classList.toggle('active')
+    })
+}))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
