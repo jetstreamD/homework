e8631d63f2ba60c664f0b97d526151a470875f83
jetstreamD <tepetovdemyan537@gmail.com>
2025-02-18 23:11:36 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -72,7 +72,40 @@ accordions.forEach(element => {
     })
 })
 
+////////////////////////////////////////////////////////////////
+/////////////////////////G a l l e r y///////////////////////////
+///////////////////////     SLIDER      //////////////////////////
 
+const swiper = new Swiper('.gallery__slider', {
+
+    spaceBetween: 15,
+    slidesPerView: 1.5,
+
+    pagination: {
+        el: '.gallery__pagination',
+        type: 'fraction',
+    },
+
+    navigation: {
+        nextEl: '.gallery__next',
+        prevEl: '.gallery__prev',
+    },
+
+    breakpoints: {
+        601: {
+            slidesPerView: 3,
+        },
+
+        801: {
+            slidesPerView: 4,
+            spaceBetween: 32
+        },
+
+        1001: {
+            slidesPerView: 4,
+        }
+    }
+});
 
 
 
