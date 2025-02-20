9a5cf303a128b5976c2dc45d61ba35bd26f82a86
jetstreamD <tepetovdemyan537@gmail.com>
2025-02-20 07:47:23 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -112,6 +112,43 @@ const swiper = new Swiper('.gallery__slider', {
     }
 });
 
+////////////////////////////////////////////////////////////////
+/////////////////////T e s t i m o n i a l s///////////////////////////
+///////////////////////     SLIDER      //////////////////////////
+
+new Swiper('.testimonials__slider', {
+
+    spaceBetween: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+
+    navigation: {
+        nextEl: '.testimonials__next',
+        prevEl: '.testimonials__prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+
+    breakpoints: {
+        
+        801: {
+            slidesPerView: 1.5,
+        },
+
+        801: {
+            slidesPerView: 1.5,
+        },
+
+        1201: {
+            slidesPerView: 2.1,
+        }
+    }
+});
+
+
 
 
 
