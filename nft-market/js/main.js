b07a5efb58e37e4b0d06ee9f053fbebe2694f3db
jetstreamD <tepetovdemyan537@gmail.com>
2025-03-03 01:20:29 +0300

    Delete nft-market directory

--- nft-market/js/main.js
+++
@@ -1,9 +0,0 @@
-// /hamburger
-////
-const hamburger = document.querySelector('.hamburger')
-const hamburgerMenu = document.querySelector('.burger__nav')
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active')
-    hamburgerMenu.classList.toggle('active')
-})
